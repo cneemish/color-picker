7c3a02951555a9f9979fe6b91257ac3524d8dd71
cneemish <cneemish@gmail.com>
2024-10-25 02:51:58 +0530

    first commit

---
+++ src/common/types/types.ts
@@ -0,0 +1,17 @@
+export interface KeyValueObj {
+  [key: string]: string;
+}
+
+export interface ColorPickerData {
+  showPicker: boolean;
+  pickerColor: {
+      r: string;
+      g: string;
+      b: string;
+      a: string;
+  };
+}     
+
+export type ChildProp = {
+  children: string | JSX.Element | JSX.Element[];
+};
